4bd5fe48dd2ce6f27a8df0327f4707704b886ec1
ero-yan07 <ehovhannisyan00@gmail.com>
2026-02-09 19:02:44 +0400

    update

--- Das33-jquery/script.js
+++ Das33-jquery/script.js
@@ -1,39 +1,23 @@
-$(function(){
-    function planetSpin1(planet){
-        $(planet).animate({
-            left: "100%"
-        },5000).css({
-            opacity: 1,
-        })
-    };    
-    function planetSpin2(planet){
-        $(planet).animate({
-            top: "100vh",
-        },5000).css({
-            opacity: 1,
-        })
-    }; 
-    function planetSpin3(planet){
-        $(planet).animate({
-            top: "100vh",
-            left: "100%"
-        },5000).css({
-            opacity: 1,
-        })
-    }; 
+$(function(){ 
     $("body").on("keyup", function(event){
         let click = event.key.toLowerCase();
         if(click === "e"){
-            planetSpin2("#earth-img");
+            $("#earth-img").animate({top: "100vh"}, 5000).css({opacity: 1});
         }
         if(click === "s"){
-            planetSpin1("#saturn-img");
+            $("#saturn-img").animate({left: "100%"}, 5000).css({opacity: 1});
         }
         if(click === "u"){
-            planetSpin3("#uran-img");
+            $("#uran-img").animate({
+                top: "100vh",
+                left: "100%"
+            }, 5000).css({opacity: 1});
         }
         if(click === "y"){
-            planetSpin2("#yupiter-img");
+            $("#yupiter-img").animate({
+                top: "100vh",
+                right: "100%"
+            }, 5000).css({opacity: 1});
         }
     });
 });
